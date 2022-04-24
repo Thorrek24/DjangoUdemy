e2f415ddf974ad715556685e3a4d73eaae26d0ad
Thorrek24 <albermarpi908@gmail.com>
2022-04-24 20:10:00 +0200

    Primer modelo de la app FCT

---
+++ pd110/src/FCT/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+
+class Registrado(models.Model):
+    nombre = models.CharField(max_length=100)#, blank=True, null=True si añadimos estos dos campos
+                                            # hacemos que no sea obligatorio rellenarlo
+    apellido = models.CharField(max_length=200)
+    email = models.EmailField()
+    timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
+
+    def __unicode__(self): #Python2
+        return self.email
+
+    def __str__(self): #Python3
+        return self.email
