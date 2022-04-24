a7650d6b5ff25bcc78dbca00135681da25571c70
Thorrek24 <albermarpi908@gmail.com>
2022-04-25 00:47:45 +0200

    Validacion de formularios y guardar datos del formulario con el Modelo

--- pd110/src/FCT/forms.py
+++ pd110/src/FCT/forms.py
@@ -3,5 +3,6 @@ from django import forms
 class SignUp(forms.Form):
     nombre = forms.CharField(max_length=100)
     apellido = forms.CharField(max_length=200)
+    email = forms.EmailField()
     edad = forms.IntegerField()
     #Puesto = forms.CheckboxSelectMultiple(choices="Estudiante" "Profesor",)
--- pd110/src/FCT/views.py
+++ pd110/src/FCT/views.py
@@ -1,11 +1,20 @@
 from multiprocessing import context
+from webbrowser import get
 from django.shortcuts import render
 
 from .forms import SignUp
+from .models import Registrado
 
 # Create your views here.
 
 def inicio(request):
-    form = SignUp()
+    form = SignUp(request.POST or None)
+    if form.is_valid():
+       form_data = form.cleaned_data
+       correo = form_data.get("email")
+       usuario = form_data.get("nombre")
+       userape = form_data.get("apellido")
+       registro = Registrado.objects.create(email=correo, nombre=usuario, apellido=userape)
+       
     context = {"the_form": form,}
     return render(request, "inicio.html", context)
